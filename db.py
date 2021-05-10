cd0e940ed7232f1ddae10a18f950cd6518ff661c
Nicholas McKibben <nicholas.bgp@gmail.com>
2021-05-09 19:28:25 -0700

    ENH: missing files and requirements.txt file

---
+++ db.py
@@ -0,0 +1,30 @@
+'''Database abstraction layer.'''
+
+import sqlite3
+import pathlib
+import logging
+logger = logging.getLogger('m2b-database')
+
+DB_FILE = 'm2b.db'
+
+
+def initialize():
+    logger.info('Starting DB initialization')
+    if pathlib.Path(DB_FILE).exists():
+        logger.info(f'Deleting {DB_FILE}')
+        pathlib.Path(DB_FILE).unlink()
+    con = sqlite3.connect(DB_FILE)
+    with con:
+        cur = con.cursor()
+
+        # Create tables
+        cur.execute("CREATE TABLE games (id INTEGER PRIMARY KEY, keyword text)")
+        cur.execute("INSERT INTO games (keyword) VALUES ('myCoolKeyword')")
+
+
+def get_games():
+    con = sqlite3.connect(DB_FILE)
+    with con:
+        cur = con.cursor()
+        cur.execute("select * from games")
+        return cur.fetchall()
